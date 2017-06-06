06626f5b4c3def421f82d39bb39ad9eda85d132d
DanielTaranger <nadenofficial@gmail.com>
2017-06-06 06:28:19 +0200

    Create poeTransactionCounter.js

---
+++ poeTransactionCounter.js
@@ -0,0 +1,94 @@
+var elements = document.getElementsByClassName("packageName el FontinBold");
+var total = 0;
+
+for(var i = 0; i < elements.length; ++i){
+    myString = elements[i].innerHTML;
+    myString = myString.trim();
+        
+        var out = 0;
+    if(myString.includes("Legacy")){
+        out = 30;
+    }else if(myString.includes("Legacy")){
+        out = 60;
+    }else if(myString.includes("Breachspawn")){
+        out = 30;
+    }else if(myString.includes("Breachlord")){
+        out = 60;
+    }else if(myString.includes("Minotaur")){
+        out = 50;
+    }else if(myString.includes("Hydra")){
+        out = 110;
+    }else if(myString.includes("Chimera")){
+        out = 220;
+    }else if(myString.includes("Phoenix")){
+        out = 440;
+    }else if(myString.includes("Prophecy")){
+        out = 60;
+    }else if(myString.includes("Aspirant")){
+        out = 50;
+    }else if(myString.includes("Challenger")){
+        out = 110;
+    }else if(myString.includes("Sovereign")){
+        out = 220;
+    }else if(myString.includes("Ascendant")){
+        out = 440;
+    }else if(myString.includes("Awakening")){
+        out = 30;
+    }else if(myString.includes("Axiom")){
+        out = 60;
+    }else if(myString.includes("Vaal")){
+        out = 130;
+    }else if(myString.includes("Lunaris")){
+        out = 260;
+    }else if(myString.includes("Highgate")){
+        out = 1100;
+    }else if(myString.includes("Apprentice")){
+        out = 50;
+    }else if(myString.includes("Journeyman")){
+        out = 100;
+    }else if(myString.includes("Grandmaster")){
+        out = 500;
+    }else if(myString.includes("Survivor")){
+        out = 50;
+    }else if(myString.includes("Warrior")){
+        out = 120;
+    }else if(myString.includes("Champion")){
+        out = 280;
+    }else if(myString.includes("Conqueror")){
+        out = 900;
+    }else if(myString.includes("Open Beta")){
+        out = 30;
+    }else if(myString.includes("Regal")){
+        out = 50;
+    }else if(myString.includes("Divine")){
+        out = 110;
+    }else if(myString.includes("Exalted")){
+        out = 270;
+    }else if(myString.includes("Eternal")){
+        out = 1500;
+    }else if(myString.includes("Ruler of Wraeclast")){
+        out = 12500;
+    }else if(myString.includes("Kiwi")){
+        out = 25;
+    }else if(myString.includes("Bronze")){
+        out = 50;
+    }else if(myString.includes("Silver")){
+        out = 100;
+    }else if(myString.includes("Gold")){
+        out = 250;
+    }else if(myString.includes("Diamond")){
+        out = 1000;
+    }else if(myString.includes("50 Point")){
+        out = 5;
+    }else if(myString.includes("100 Point")){
+        out = 10;
+    }else if(myString.includes("200 Point")){
+        out = 20;
+    }else{
+    }
+        
+    console.log(myString);
+    console.log(total + "+"+ out + "=" + (total = total + parseInt(out)));  
+}
+
+alert("You have spent " + "$" + total + " on microtransactions");
